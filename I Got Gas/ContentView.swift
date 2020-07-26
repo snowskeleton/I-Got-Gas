62a53bafa13d0dd5f555b6e5ba537293f13e979d
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-07-26 16:43:05 -0400

    Can't seem to figure out how to stop the app from crashing when it tries to display a nil value.

--- I Got Gas/ContentView.swift
+++ I Got Gas/ContentView.swift
@@ -11,19 +11,39 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.managedObjectContext) var managedObjectContext
     @FetchRequest(entity: Car.entity(), sortDescriptors: []) var cars: FetchedResults<Car>
-
+    
     var body: some View {
         VStack {
             TopMenuBarView()
-            Spacer()
-            CarView(name: "Julie", make: "Jeep", model: "Rangler", year: "2010")
-            Spacer()
+                .padding(.bottom)
+            NavigationView {
+                VStack {
+                    List {
+                        ForEach(cars, id: \.self) { car in
+                            CarView(name: car.name!, make: car.make!, model: car.model!, year: car.year!)
+                        }
+                    }
+                    //                CarView(name: "Julie", make: "Jeep", model: "Rangler", year: "2010")
+                    //                Spacer()
+                }
+            }
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        //Test data
+        let car = Car.init(context: context)
+        car.name = ""
+        car.year = ""
+        car.make = ""
+        car.model = ""
+        car.plate = ""
+        car.vin = ""
+        return ContentView().environment(\.managedObjectContext, context)
+        
+        //        ContentView()
     }
 }
