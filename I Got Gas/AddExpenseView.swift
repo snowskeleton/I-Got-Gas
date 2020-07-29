7a5f06719cc5ec3d06b97e926e236fc51016da05
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-07-28 21:58:53 -0400

    Fighting with number formatting, and still undecided on how I want the expense page laid out.

--- I Got Gas/AddExpenseView.swift
+++ I Got Gas/AddExpenseView.swift
@@ -12,26 +12,55 @@ struct AddExpenseView: View {
     @Environment(\.presentationMode) var presentationMode
     var fetchRequest: FetchRequest<Car>
     var car: FetchedResults<Car> { fetchRequest.wrappedValue }
-
+    
+    @State private var gasPrice: Decimal = 0.00
+    @State private var gallonsOfGas: Decimal = 0.00
+    @State private var pricePerGallon: Decimal = 0.00
+    @State private var isGas = true
+    
     init(filter: String) {
-
+        
         fetchRequest = FetchRequest<Car>(entity: Car.entity(),
                                          sortDescriptors: [],
                                          predicate: NSPredicate(
                                             format: "idea BEGINSWITH %@", filter))
     }
-
+    
     var body: some View {
-        VStack {
-            ForEach(car, id: \.self) { car in
-                Text("\(car.name ?? "")")
-            }
-            
-            Spacer()
-            Button(action: {
-                self.presentationMode.wrappedValue.dismiss()
-            }) {
-                Text("Save me!")
+        ForEach(car, id: \.self) { car in
+            VStack {
+                HStack {
+                    Button(action: {
+                        self.isGas.toggle()
+                    }) {
+                        self.isGas ? Text("Gas") : Text("Service")
+                    }
+                    .font(.system(size: 30))
+                    .padding()
+                    
+                }
+                
+                NavigationView {
+                    VStack {
+                        Form {
+                            if self.isGas {
+                                Section {
+                                    TextField("Total Gallons", value: self.$gallonsOfGas, formatter: NumberFormatter())
+                                        .keyboardType(.asciiCapableNumberPad)
+                                    }
+                            }
+                            Text("\(car.name ?? "")")
+                        }
+                        
+                        Spacer()
+                        Button(action: {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }) {
+                            Text("Save me!")
+                        }
+                    }.navigationBarTitle("")
+                    .navigationBarHidden(true)
+                }
             }
         }
     }
