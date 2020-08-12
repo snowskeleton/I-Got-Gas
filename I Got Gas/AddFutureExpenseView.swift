8379958fab9590c1103424565f2eac9bf15848a3
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-08-12 13:08:15 -0400

    Added file.

---
+++ I Got Gas/AddFutureExpenseView.swift
@@ -0,0 +1,21 @@
+//
+//  AddFutureExpenseView.swift
+//  I Got Gas
+//
+//  Created by Isaac Lyons on 8/12/20.
+//  Copyright © 2020 Blizzard Skeleton. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddFutureExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddFutureExpenseView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddFutureExpenseView()
+    }
+}
