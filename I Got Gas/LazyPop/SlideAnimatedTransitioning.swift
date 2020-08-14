3eb875a5b0eceabd4ded424ee90d6b7d41265e1e
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-08-14 13:20:55 -0400

    Lazy pop.

---
+++ I Got Gas/LazyPop/SlideAnimatedTransitioning.swift
@@ -0,0 +1,9 @@
+//
+//  SlideAnimatedTransitioning.swift
+//  I Got Gas
+//
+//  Created by Isaac Lyons on 8/13/20.
+//  Copyright © 2020 Blizzard Skeleton. All rights reserved.
+//
+
+import Foundation
