b3a0628aafe3503111247e11182a19528b92e316
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-08-23 21:02:01 -0400

    Added OptionsView

---
+++ I Got Gas/OptionsView.swift
@@ -0,0 +1,21 @@
+//
+//  OptionsView.swift
+//  I Got Gas
+//
+//  Created by Isaac Lyons on 8/23/20.
+//  Copyright © 2020 Blizzard Skeleton. All rights reserved.
+//
+
+import SwiftUI
+
+struct OptionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OptionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        OptionsView()
+    }
+}
