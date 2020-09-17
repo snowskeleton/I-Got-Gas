732ac615d9300ef56bd91973a48c1881afad9488
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-09-17 11:03:22 -0400

    Added file.

---
+++ I Got Gas/AboutView.swift
@@ -0,0 +1,16 @@
+//
+//  AboutView.swift
+//  I Got Gas
+//
+//  Created by Isaac Lyons on 9/17/20.
+//  Copyright © 2020 Blizzard Skeleton. All rights reserved.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
