2681f805e110fe14c0bb0890eec083ad9aae54b9
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-09-17 12:26:37 -0400

    Privacy Policy update.

--- I Got Gas/PrivacyPolicyView.swift
+++ I Got Gas/PrivacyPolicyView.swift
@@ -9,8 +9,29 @@
 import SwiftUI
 
 struct PrivacyPolicyView: View {
+    @Environment(\.colorScheme) var colorScheme
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Privacy Policy")
+                .padding()
+                .font(.largeTitle)
+                .foregroundColor(colorScheme == .dark
+                                    ? Color.white
+                                    : Color.black)
+
+            Spacer()
+            
+            Text("For now, this app does not collect any user data, or any other data of any kind. This is subject to change at any time.\n\nIf/when there is a change to our data collection practices, be assured that user privacy will have the utmost consideration.\n\nAny data collected in the future will be used soley for improvements in this app, and will never be sold to any third party.")
+                .foregroundColor(colorScheme == .dark
+                                    ? Color.white
+                                    : Color.black)
+                .fontWeight(.light)
+                .padding(.leading)
+                .padding(.trailing)
+            
+            Spacer()
+        }
     }
 }
 
