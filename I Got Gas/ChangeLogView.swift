dcf5c491e587760a2f691b9d49143c4d8e930b9b
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-09-17 12:32:18 -0400

    Added file.

---
+++ I Got Gas/ChangeLogView.swift
@@ -0,0 +1,21 @@
+//
+//  ChangeLogView.swift
+//  I Got Gas
+//
+//  Created by Isaac Lyons on 9/17/20.
+//  Copyright © 2020 Blizzard Skeleton. All rights reserved.
+//
+
+import SwiftUI
+
+struct ChangeLogView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ChangeLogView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChangeLogView()
+    }
+}
