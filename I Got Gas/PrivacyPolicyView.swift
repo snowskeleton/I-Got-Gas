829a931f874bdb68281ab735c0b81b24ffa195ca
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-09-17 11:11:42 -0400

    Added.

---
+++ I Got Gas/PrivacyPolicyView.swift
@@ -0,0 +1,21 @@
+//
+//  PrivacyPolicyView.swift
+//  I Got Gas
+//
+//  Created by Isaac Lyons on 9/17/20.
+//  Copyright © 2020 Blizzard Skeleton. All rights reserved.
+//
+
+import SwiftUI
+
+struct PrivacyPolicyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PrivacyPolicyView_Previews: PreviewProvider {
+    static var previews: some View {
+        PrivacyPolicyView()
+    }
+}
