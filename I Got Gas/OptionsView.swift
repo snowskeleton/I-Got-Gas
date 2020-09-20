a2ba068eea4a521dc7dd4a0bc827c52ea338eb34
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-09-19 23:09:10 -0400

    Ads are now testing. Once done testing, need to replace the add unit id with a real one. Gotta make that money.

--- I Got Gas/OptionsView.swift
+++ I Got Gas/OptionsView.swift
@@ -14,6 +14,10 @@ struct OptionsView: View {
     @State private var formatSelection: Int
     @State var showAboutView = false
     var formatList: [String]
+    //when done testing add placement, take out from here
+    var topOrBottomList: [Bool]
+    @State private var selectTopOrBottom: Int
+    //to here
     
     init() {
         formatList = ["%.3f", "%.2f"]
@@ -23,6 +27,17 @@ struct OptionsView: View {
         } else {
             _formatSelection = State<Int>(initialValue: formatList.firstIndex(of: priceFormat!)!)
         }
+
+
+        //when done testing add placement, take out from here
+        topOrBottomList = [true, false]
+        let adOnTop = UserDefaults.standard.bool(forKey: "isAdOnTop")
+        if adOnTop == true {
+            _selectTopOrBottom = State<Int>(initialValue: 0)
+        } else {
+            _selectTopOrBottom = State<Int>(initialValue: 1)
+        }
+        //to here
     }
     
     var body: some View {
@@ -39,6 +54,24 @@ struct OptionsView: View {
                             UserDefaults.standard.set(formatList[formatSelection],
                                                       forKey: "priceFormat")
                         }
+                        //when done testing add placement, take out from here
+                        Picker(selection: $selectTopOrBottom,
+                               label: Text("Where is your ad?")) {
+                            Text("Ad on Top").tag(0)
+                            Text("Ad on Bottom").tag(1)
+                            }
+                        }.pickerStyle(SegmentedPickerStyle())
+                        .onChange(of: selectTopOrBottom) { _ in
+                            if selectTopOrBottom == 0 {
+                                UserDefaults.standard.set(true,
+                                                          forKey: "isAdOnTop")
+                            } else {
+                                UserDefaults.standard.set(false,
+                                                          forKey: "isAdOnTop")
+                            }
+                            fatalError()
+                        }
+                        // to here
                     }
                     Section {
                         Button(action: {
@@ -62,5 +95,4 @@ struct OptionsView: View {
             .navigationBarTitle(Text("Options"))
         }
     }
-}
 
---
+++ Pods/Target Support Files/Pods-I Got Gas/Pods-I Got Gas-dummy 2.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_I_Got_Gas : NSObject
+@end
+@implementation PodsDummy_Pods_I_Got_Gas
+@end
