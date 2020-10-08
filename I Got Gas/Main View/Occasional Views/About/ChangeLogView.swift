d94c90417ab202327cd9b07458e0e3785928f7c7
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-10-08 12:53:22 -0400

    Updated change log.

--- I Got Gas/Main View/Occasional Views/About/ChangeLogView.swift
+++ I Got Gas/Main View/Occasional Views/About/ChangeLogView.swift
@@ -11,6 +11,11 @@ import SwiftUI
 struct ChangeLogView: View {
     var body: some View {
         Form {
+
+            Section(header: Text("Version 1.0.1")) {
+                Text("  - Added a Support button that will let you send me an email.\n\n    - Made the change log pretty (can you tell?).\n\n")
+            }
+
             Section(header: Text("Version 1.0.0")) {
                 Text("First public release! More changes to come.\n\nI Got Gas, in its current form, is designed to let you keep track of how much your vehicle is costing you mile by mile. Future releases will make it even more granular and add new features.")
             }
