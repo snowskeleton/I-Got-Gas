5b3588f1f1dbbc40c7f32cd55ec3f275ba953fe8
snowskeleton <snowskeleto@blizzardskeleton.net>
2020-10-10 14:08:26 -0400

    Updated change log.

--- I Got Gas/ContentView/Options View/About View/ChangeLogView.swift
+++ I Got Gas/ContentView/Options View/About View/ChangeLogView.swift
@@ -14,11 +14,6 @@ struct ChangeLogView: View {
     var body: some View {
         Form {
 
-            Section(header: Text("Version 1.0.2")) {
-                Text("- The price field in the Add Expense page now automatically adds a decimal point for you.\n\n- Centered a label that should have been centered.\n\n- Added an arrow to the Repeating/One Time toggle in the Futre Expense scheduler. Hopefully this makes it obvious that it's a toggle")
-                    .fontWeight(.light)
-            }
-
             Section(header: Text("Version 1.0.1")) {
                 Text("- Added a Support button that will let you send me an email.\n\n- Made the change log pretty (can you tell?).\n\n- Prevented random elements from turning blue for no reason.\n\n- Gave all the boxes a shadow to help them stand out. Also added some spacing to give them room to breathe.")
                     .fontWeight(.light)
