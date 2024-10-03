7ef6a4e712bf50b6af1a782310a3641c50e89e84
Isaac Lyons <isaac@snowskeleton.net>
2024-10-03 16:03:08 -0400

    reflow with modern SwiftUI

--- I Got Gas/Content View/AddCarView.swift
+++ I Got Gas/Content View/AddCarView.swift
@@ -14,92 +14,63 @@ struct AddCarView: View {
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(entity: Car.entity(), sortDescriptors: []) var cars: FetchedResults<Car>
     
-    @State private var buttonEnabled = false
-    
-    @State private var carYear: String = ""
-    
     @State private var carMake = ""
     @State private var carModel = ""
     @State private var carPlate = ""
     @State private var carVIN = ""
     @State private var carOdometer = ""
         
-    var years = yearsPlusTwo()
-    @State var selectionIndex = 0
+    @State var carYear: String
+    let yearRange: [String]
+    
+    var buttonDisabled: Bool {
+        carMake.isEmpty ||
+        carModel.isEmpty ||
+        carPlate.isEmpty ||
+        carVIN.isEmpty ||
+        carOdometer.isEmpty
+    }
+
+    init() {
+        let currentYear = Calendar.current.component(.year, from: Date())
+        _carYear = .init(initialValue: currentYear.description)
+        self.yearRange = (1990...currentYear+2).map { String($0) }.reversed()
+    }
     
     var body: some View {
         VStack {
             NavigationView {
                 VStack {
                     Form {
-                        Section(header: Text("Vehicle Info"), footer: Text("\nTo increase accuracy of results, it's recommended to only add a new car when it has a full tank of gas.")) {
-                            
-                            TextFieldWithPickerAsInputView(data: self.years,
-                                                           placeholder: "* Year",
-                                                           selectionIndex: self.$selectionIndex,
-                                                           text: self.$carYear)
-                                .onChange(of: self.carYear) { _ in
-                                    self.maybeEnableButton()
+                        Section(
+                            header: Text("Vehicle Info"),
+                            footer: Text("\nTo increase accuracy of results, it's recommended to only add a new car when it has a full tank of gas.")
+                        ) {
+                            Picker("Year", selection: $carYear) {
+                                ForEach(yearRange, id: \.self) { year in
+                                    Text(year).tag(year)
                                 }
-                            
-                            TextField("* Make",
-                                      text: self.$carMake,
-                                      onCommit: {
-                                        self.maybeEnableButton()
-                                    })
-                                .onChange(of: self.carMake) { _ in
-                                    self.maybeEnableButton()
-                                }
+                            }
 
-                            TextField("* Model",
-                                      text: self.$carModel,
-                                      onCommit: {
-                                        self.maybeEnableButton()
-                                    })
-                                .onChange(of: self.carModel) { _ in
-                                    self.maybeEnableButton()
-                                }
+                            TextField("Make", text: $carMake)
 
-                            TextField("* Current Odometer",
-                                      text: self.$carOdometer,
-                                      onCommit: {
-                                        self.maybeEnableButton()
-                                    })
+                            TextField("Model", text: $carModel)
+
+                            TextField("Current Odometer", text: self.$carOdometer)
                                 .keyboardType(.numberPad)
-                                .onChange(of: self.carOdometer) { _ in
-                                    self.maybeEnableButton()
-                                }
 
-                            TextField("License Plate",
-                                      text: self.$carPlate,
-                                      onCommit: {
-                                        self.maybeEnableButton()
-                                    })
+                            TextField("License Plate (Optional)", text: self.$carPlate)
                                 .disableAutocorrection(true)
-                                .onChange(of: self.carPlate) { _ in
-                                    self.maybeEnableButton()
-                                }
 
-                            TextField("VIN",
-                                      text: self.$carVIN,
-                                      onCommit: {
-                                        self.maybeEnableButton()
-                                    })
+                            TextField("VIN (Optional)", text: self.$carVIN)
                                 .disableAutocorrection(true)
-                                .onChange(of: self.carVIN) { _ in
-                                    self.maybeEnableButton()
-                                }
                         }
                         
                         Section {
-                            Button(action: {
-                                self.save()
-                                self.presentationMode.wrappedValue.dismiss()
-                            }) {
-                                Text("Add Vehicle")
+                            Button("Add Vehicle") {
+                                save()
                             }
-                            .disabled(!buttonEnabled)
-                            .buttonStyle(PlainButtonStyle())
+                            .disabled(buttonDisabled)
                         }
                     }
                 }
@@ -108,36 +79,17 @@ struct AddCarView: View {
         }
     }
     
-    func maybeEnableButton() {
-        if self.carYear == "" {
-            self.buttonEnabled = false
-            return
-        }
-        if self.carMake == "" {
-            self.buttonEnabled = false
-            return
-        }
-        if self.carModel == "" {
-            self.buttonEnabled = false
-            return
-        }
-        if self.carOdometer == "" {
-            self.buttonEnabled = false
-            return
-        }
-        self.buttonEnabled = true
-    }
-    
     func save() {
-        let car = Car(context: self.moc)
-        car.year = self.carYear
-        car.make = self.carMake
-        car.model = self.carModel
-        car.plate = self.carPlate
-        car.vin = self.carVIN
-        car.odometer = Int64(self.carOdometer)!
-        car.startingOdometer = Int64(self.carOdometer)!
+        let car = Car(context: moc)
+        car.year = carYear
+        car.make = carMake
+        car.model = carModel
+        car.plate = carPlate
+        car.vin = carVIN
+        car.odometer = Int64(carOdometer)!
+        car.startingOdometer = Int64(carOdometer)!
         car.id = UUID().uuidString
-        try? self.moc.save()        
+        try? moc.save()
+        presentationMode.wrappedValue.dismiss()
     }
 }
